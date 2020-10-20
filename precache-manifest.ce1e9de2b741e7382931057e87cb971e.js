420122190f6f2ad5f1ded74fc259b74b63d2d711
texas2010 <9701609+texas2010@users.noreply.github.com>
2020-10-19 20:20:19 -0500

    Updates

---
+++ precache-manifest.ce1e9de2b741e7382931057e87cb971e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bc85f227a68a266e1bb76b71fe9ad9",
+    "url": "/the-countdown-clock/index.html"
+  },
+  {
+    "revision": "41852ccb94cdb7d09592",
+    "url": "/the-countdown-clock/static/css/main.520abdc3.chunk.css"
+  },
+  {
+    "revision": "9553afebb8e6e703178e",
+    "url": "/the-countdown-clock/static/js/2.ddb03e7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-countdown-clock/static/js/2.ddb03e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41852ccb94cdb7d09592",
+    "url": "/the-countdown-clock/static/js/main.5e204eff.chunk.js"
+  },
+  {
+    "revision": "00ae2450f24fe6caefe4",
+    "url": "/the-countdown-clock/static/js/runtime-main.c9a13fe2.js"
+  }
+]);
